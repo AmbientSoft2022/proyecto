11f5f705ec597aa8ea0e168125c9b599fe457cbf
AmbientSoft2022 <ambientsoft2022@gmail.com>
2022-10-01 12:38:30 -0500

    cambios

--- src/main/java/com/AmbientSoft/main/AmbientSoftApplication.java
+++ src/main/java/com/AmbientSoft/main/AmbientSoftApplication.java
@@ -2,8 +2,10 @@ package com.AmbientSoft.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.AmbientSoft.main.controlador"})
 public class AmbientSoftApplication {
 
 	public static void main(String[] args) {
