7c1088fb20889ca962d4bafe903675778c7d4cd9
AmbientSoft2022 <ambientsoft2022@gmail.com>
2022-10-01 11:42:08 -0500

    actualizacion IJ

--- src/main/java/com/AmbientSoft/main/AmbientSoftApplication.java
+++ src/main/java/com/AmbientSoft/main/AmbientSoftApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AmbientSoftApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(AmbientSoftApplication.class, args);
 	}
